1cf3c32630ed5ee6a44dcedea24ba0f73f7b2ecf
Pradeep <prdp.urs@gmail.com>
2024-06-17 22:10:58 +0800

    ecommerce microservice basic structure

---
+++ product-catalog-service/src/main/java/com/general/productcatalog/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.general.productcatalog.controller;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/testcheckMS")
+    public String testCheckMS(){
+        return "Test check Rest API MS success..!";
+    }
+
+    @QueryMapping
+    public String testCheckMSGQL(){
+        return "Test testCheckGQL MS success..!";
+    }
+
+}
